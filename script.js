ea2456871f4df9388546ca952ebc1d3a28e81a01
Katie Fritz <katie@katiemfritz.com>
2015-10-03 15:38:53 -0400

    add an array called jokes to a script.js file and take js out of index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+  var jokes = ["How do you make a Kleenex dance? Put a little boogie in it!", "Two guys walk into a bar, the third one ducks.", "I had a dream that I was a muffler last night. I woke up exhausted!", "Why can’t you hear a pterodactyl use the bathroom? Because the P is silent."];
+</script>
