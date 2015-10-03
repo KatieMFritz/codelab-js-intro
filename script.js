ea7448040d08b3ede728f9a535485377d8aae4a1
Katie Fritz <katie@katiemfritz.com>
2015-10-03 16:33:28 -0400

    getjoke function displays a random joke (but have to refresh the page to get a new one

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 var jokes = ["How do you make a Kleenex dance? Put a little boogie in it!", "Two guys walk into a bar, the third one ducks.", "I had a dream that I was a muffler last night. I woke up exhausted!", "Why can’t you hear a pterodactyl use the bathroom? Because the P is silent."];
 
-function randomjoke() {
+var randomjoke = jokes[Math.floor(Math.random() * jokes.length)];
 
- document.getElementById('getjoke') = jokes[Math.floor(Math.random() * jokes.length)];
-
-}
-
-function testfunction() {
-  alert("Here's a joke");
+function getjoke() {
+  alert(randomjoke);
 }
